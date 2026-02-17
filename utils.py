a5750061dda7cbf898f437001373888a2f866b56
treesamal41 <treesamal41@gmail.com>
2026-02-17 18:09:29 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -70,84 +70,7 @@ async def is_subscribed(bot, query=None, userid=None):
 
     return invite_links
 
-async def get_poster(query, bulk=False, id=False, file=None):
-    if not id:
-        # https://t.me/GetTGLink/4183
-        query = (query.strip()).lower()
-        title = query
-        year = re.findall(r'[1-2]\d{3}$', query, re.IGNORECASE)
-        if year:
-            year = list_to_str(year[:1])
-            title = (query.replace(year, "")).strip()
-        elif file is not None:
-            year = re.findall(r'[1-2]\d{3}', file, re.IGNORECASE)
-            if year:
-                year = list_to_str(year[:1]) 
-        else:
-            year = None
-        movieid = imdb.search_movie(title.lower(), results=10)
-        if not movieid:
-            return None
-        if year:
-            filtered=list(filter(lambda k: str(k.get('year')) == str(year), movieid))
-            if not filtered:
-                filtered = movieid
-        else:
-            filtered = movieid
-        movieid=list(filter(lambda k: k.get('kind') in ['movie', 'tv series'], filtered))
-        if not movieid:
-            movieid = filtered
-        if bulk:
-            return movieid
-        movieid = movieid[0].movieID
-    else:
-        movieid = query
-    movie = imdb.get_movie(movieid)
-    if movie.get("original air date"):
-        date = movie["original air date"]
-    elif movie.get("year"):
-        date = movie.get("year")
-    else:
-        date = "N/A"
-    plot = ""
-    if not LONG_IMDB_DESCRIPTION:
-        plot = movie.get('plot')
-        if plot and len(plot) > 0:
-            plot = plot[0]
-    else:
-        plot = movie.get('plot outline')
-    if plot and len(plot) > 800:
-        plot = plot[0:800] + "..."
 
-    return {
-        'title': movie.get('title'),
-        'votes': movie.get('votes'),
-        "aka": list_to_str(movie.get("akas")),
-        "seasons": movie.get("number of seasons"),
-        "box_office": movie.get('box office'),
-        'localized_title': movie.get('localized title'),
-        'kind': movie.get("kind"),
-        "imdb_id": f"tt{movie.get('imdbID')}",
-        "cast": list_to_str(movie.get("cast")),
-        "runtime": list_to_str(movie.get("runtimes")),
-        "countries": list_to_str(movie.get("countries")),
-        "certificates": list_to_str(movie.get("certificates")),
-        "languages": list_to_str(movie.get("languages")),
-        "director": list_to_str(movie.get("director")),
-        "writer":list_to_str(movie.get("writer")),
-        "producer":list_to_str(movie.get("producer")),
-        "composer":list_to_str(movie.get("composer")) ,
-        "cinematographer":list_to_str(movie.get("cinematographer")),
-        "music_team": list_to_str(movie.get("music department")),
-        "distributors": list_to_str(movie.get("distributors")),
-        'release_date': date,
-        'year': movie.get('year'),
-        'genres': list_to_str(movie.get("genres")),
-        'poster': movie.get('full-size cover url'),
-        'plot': plot,
-        'rating': str(movie.get("rating")),
-        'url':f'https://www.imdb.com/title/tt{movieid}'
-    }
 # https://github.com/odysseusmax/animated-lamp/blob/2ef4730eb2b5f0596ed6d03e7b05243d93e3415b/bot/utils/broadcast.py#L37
 
 async def broadcast_messages(user_id, message):
@@ -231,6 +154,43 @@ def get_file_id(msg: Message):
 
 def extract_user(message: Message) -> Union[int, str]:
     """extracts the user from a message"""
+    poster = (
+    movie.get('cover url')
+    or movie.get('full-size cover url')
+)
+
+if poster:
+    poster = poster.replace("http://", "https://")
+
+return {
+    'title': movie.get('title'),
+    'votes': movie.get('votes'),
+    "aka": list_to_str(movie.get("akas")),
+    "seasons": movie.get("number of seasons"),
+    "box_office": movie.get('box office'),
+    'localized_title': movie.get('localized title'),
+    'kind': movie.get("kind"),
+    "imdb_id": f"tt{movie.get('imdbID')}",
+    "cast": list_to_str(movie.get("cast")),
+    "runtime": list_to_str(movie.get("runtimes")),
+    "countries": list_to_str(movie.get("countries")),
+    "certificates": list_to_str(movie.get("certificates")),
+    "languages": list_to_str(movie.get("languages")),
+    "director": list_to_str(movie.get("director")),
+    "writer": list_to_str(movie.get("writer")),
+    "producer": list_to_str(movie.get("producer")),
+    "composer": list_to_str(movie.get("composer")),
+    "cinematographer": list_to_str(movie.get("cinematographer")),
+    "music_team": list_to_str(movie.get("music department")),
+    "distributors": list_to_str(movie.get("distributors")),
+    'release_date': date,
+    'year': movie.get('year'),
+    'genres': list_to_str(movie.get("genres")),
+    'poster': poster,
+    'plot': plot,
+    'rating': str(movie.get("rating")),
+    'url': f'https://www.imdb.com/title/tt{movieid}'
+}
     # https://github.com/SpEcHiDe/PyroGramBot/blob/f30e2cca12002121bad1982f68cd0ff9814ce027/pyrobot/helper_functions/extract_user.py#L7
     user_id = None
     user_first_name = None
